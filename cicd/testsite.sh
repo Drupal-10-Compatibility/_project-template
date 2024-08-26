dee5b084c8d0983f15236ba8d2eb3423c3b03955
Lisa Ridley <l.ridley@vardot.com>
2024-08-26 13:50:30 -0400

    Updating test_installation workflow and related bash script

--- cicd/testsite.sh
+++ cicd/testsite.sh
@@ -3,10 +3,21 @@
 # Tests the site after installing and starting the PHP server
 # To make sure it returns a 200 response code.
 
+# Site requires an input variable, which is the connection string
+# used to establish the database connection for conducting a site
+# intallation.
+
+# For Github Actions, this is passed in from the job, and is set in
+# a variable in the calling action.
+
 set -e
 
+SIMPLETEST_DB=$1
+
 ## Installs site from existing configuration.
-./vendor/bin/drush @self -y si --existing-config
+./vendor/bin/drush @self -yes si --existing-config --db-url=$SIMPLETEST_DB
+## If doing a fresh install without existing config
+# ./vendor/bin/drush drush site-install standard --yes --account-pass=admin --db-url=$SIMPLETEST_DB
 ## Start php server with the default drupal router, and capture job ID.
 nohup php -S 127.0.0.1:8888 ./cicd/.htrouter.php &> ./tmp/server.log &
 PHP_SERVER_PID=$!
@@ -19,7 +30,7 @@ OUTPUT=$(curl -s -o /dev/null -I -w "%{http_code}" 127.0.0.1:8888)
 ## PHP server and exits the script, returning 0 exit code if server return
 ## a 200, or a 1 exit code if the server returns a different response code.
 if [[ "$OUTPUT" == 200 ]]; then
-  echo "Test Successful, site loaded!"
+  echo "Test Successful, site loaded!";
   kill -9 $PHP_SERVER_PID;
   exit 0;
 else
